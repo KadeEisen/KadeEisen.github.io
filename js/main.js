eaed2b73ef5f124f495c7cf8563a4a7c3303e6e4
KadeEisen <keei3508@colorado.edu>
2022-04-03 19:10:31 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-document.getElementById("button").addEventListener("click", troubleshoot);
+document.getElementById("button").addEventListener("click", buttonClicked);
 document.getElementById("buttonManners").addEventListener("click", buttonClickedManners);
-function troubleshoot()
-{
-  alert("The button has been clicked!");
-}
+document.getElementById("game").addEventListener("mouseover", dare);
 
 function buttonClicked()
 {
-
+  alert("There is no game.");
 }
 
 function buttonClickedManners()
 {
+  document.getElementById("game").innerHTML = "I dare you to hover over this.";
+}
 
+function dare()
+{
+  document.getElementById("game").innerHTML = "You just took orders from a computer.";
 }

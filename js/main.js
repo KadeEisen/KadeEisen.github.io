5afcddf1d1bfa9247e41e64f4f691b5c8683e4fa
KadeEisen <keei3508@colorado.edu>
2022-04-30 16:58:03 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-document.getElementById("button").addEventListener("click", buttonClicked);
-document.getElementById("buttonManners").addEventListener("click", buttonClickedManners);
-document.getElementById("game").addEventListener("mouseover", dare);
+// document.getElementById("button").addEventListener("click", buttonClicked);
+// document.getElementById("buttonManners").addEventListener("click", buttonClickedManners);
+// document.getElementById("game").addEventListener("mouseover", dare);
 
+//write array of websites
+const websites = [
+  'http://corndog.io/'
+  'https://tacospin.com/'
+  'https://pointerpointer.com/'
+  'https://cat-bounce.com/'
+  'https://anotheruseless.website/ducksarethebest-com/'
+  'https://thequietplaceproject.xyz/thequietplace'
+  'https://procatinator.com/'
+]
 function buttonClicked()
 {
   alert("There is no game.");
 }
 
-function buttonClickedManners()
-{
-  document.getElementById("game").innerHTML = "I dare you to hover over this.";
+//write function to insert new url using setAttribute() DOM element
+function changeURL(){
+  // load websites[math.Random()] using setAttribute()
+  // document.getElementById("askldjfahskdf").setAttribute("href", websites[math.random()])
 }
 
-function dare()
-{
-  document.getElementById("game").innerHTML = "You just took orders from a computer.";
-}
+
+
+document.getElementById("getWebsite").addEventListener("click", buttonClicked);
